76bc3cc9546afc544a5d05ad809de3e2e73b105d
Omar Khaliq <omargraphene@gmail.com>
2018-07-18 15:28:54 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  counter = 0
+  board.each do |board_position|
+    if board_position == "X" ||  board_position == "O"
+      counter +=1
+    end
+  end
+  return counter
+end
+
+def current_player
+end
